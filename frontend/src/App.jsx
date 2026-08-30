43f89796d3e61282658e5a776e71c6617591f4f9
Faiz Khan <faizurbtech@gmail.com>
2026-08-30 14:45:05 +0530

    Scroll to top feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,12 +38,13 @@ import OrderManagement from "./Admin/OrderManagement";
 import ShopManagement from "./Admin/ShopManagement";
 
 import { useLanguageDirection } from "./hooks/useLanguageDirection";
-
+import ScrollToTop from "./Components/ScrollToTop";
 function App() {
   useLanguageDirection();
   return (
     <Provider store={store}>
       <BrowserRouter>
+        <ScrollToTop />
         <Toaster position="top-right" />
 
         <Routes>
